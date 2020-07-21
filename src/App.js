c4fcce056cde2291968a864287e8090a6872bbc3
Guilherme Figueiredo <gfigueiredo@actioncargo.com.br>
2020-07-21 10:17:19 -0300

    Aprendendo Hooks no React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+
+export default function App() {
+  const [location,setLocation] = useState({})
+
+  useEffect(()=>{
+    const idEvent = navigator.geolocation.watchPosition(handlePositionReceived)
+
+    // will mount com hooks
+    return ()=> navigator.geolocation.clearWatch(idEvent)
+
+  },[])
+
+  function handlePositionReceived({coords}){
+    const {latitude,longitude} = coords
+    setLocation({latitude,longitude })
+  }
+
+  return(
+    <>
+    Latidude: {location.latitude}<br/>
+    Longitude: {location.longitude}
+    </>
+  )
+
+  /*
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    async function loadRepo(){
+      const response = await fetch("https://api.github.com/users/Pride7K/repos")
+      const data = await response.json();
+      setRepository(data);
+    }
+    loadRepo();
+  }, [])
+
+  useEffect(()=>{
+    const filtered = repositories.filter(repo=>repo.favorite);
+    document.title = `Você tem ${filtered.length} Favoritos`
+  },[repositories])
+
+
+  function handleFavorite(id){
+    const newRepositores = repositories.map(repo => {
+      return repo.id === id ? { ...repo,favorite:!repo.favorite} : repo
+     
+    })
+    setRepository(newRepositores);
+  }
+  
+   // arrow function no button garante que o metodo não seja chamado automaticamente
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul>
+        {repositories.map(repository => <li key={repository.id}>{repository.name}
+        {repository.favorite === true ? <span> (Favorito)</span> : ""}
+        <button onClick={()=> handleFavorite(repository.id)}>Favoritar</button></li>)}
+      </ul>
+    </>
+  )
+  */
 }
 
-export default App;
